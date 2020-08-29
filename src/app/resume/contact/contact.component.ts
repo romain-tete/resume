debdc2abc10ced269a0dc39d20d89723af46e89d
Romain Tête <romain.tete@gmail.com>
2020-08-29 18:56:23 +0200

    Contact component + small changes

---
+++ src/app/resume/contact/contact.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'xa-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+  contactShown = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showContact() {
+    this.contactShown = true;
+  }
+
+}
--- src/app/resume/resume.module.ts
+++ src/app/resume/resume.module.ts
@@ -4,9 +4,11 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResumeComponent } from './resume.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
-  declarations: [ResumeComponent],
+  declarations: [ResumeComponent, ContactComponent],
   imports: [CommonModule, ResumeRoutingModule, SharedModule, ExperienceModule],
 })
 export class ResumeModule {}
