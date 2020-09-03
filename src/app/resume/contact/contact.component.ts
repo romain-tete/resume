61d1c3862498018c68f94c87c3c118d6aa7b35ca
Romain Tête <romain.tete@gmail.com>
2020-09-03 10:28:07 +0200

    Add phone number to resume on localhost

--- src/app/resume/contact/contact.component.ts
+++ src/app/resume/contact/contact.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'xa-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
   contactShown = false;
 
   constructor() {}
 
-  ngOnInit(): void {}
-
-  showContact() {
+  showContact(): void {
     this.contactShown = true;
+  }
+
+  isOnline(): boolean {
+    return window.location.hostname !== 'localhost';
   }
 }
