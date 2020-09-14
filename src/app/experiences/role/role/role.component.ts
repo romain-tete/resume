47ec08b24cb758dcb9a6c03fa498abe10f979b82
Romain Tête <romain.tete@gmail.com>
2020-09-14 11:55:12 +0200

    Add experiences page display hierarchy

--- src/app/experiences/role/role/role.component.ts
+++ src/app/experiences/role/role/role.component.ts
@@ -1,10 +1,16 @@
 import { Role } from '@xcedia/experiences';
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'xa-role',
   templateUrl: './role.component.html',
   styleUrls: ['./role.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RoleComponent implements OnInit {
   @Input() role: Role;
