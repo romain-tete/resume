69855e319a69e0d8c2650cd9181b91b6aa0e8a16
Romain Tête <romain.tete@gmail.com>
2020-09-22 14:58:26 +0200

    Add root resource creation button

--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -2,7 +2,11 @@ import { NODE_IDENTITY_FN } from '../shared/tree/tree.types';
 import { ResourceComponent } from './resource/resource.component';
 import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
-import { experienceActions as actions } from '@xcedia/experiences';
+import {
+  experienceActions as actions,
+  factories,
+  getFactory,
+} from '@xcedia/experiences';
 import { TreeNode } from '../shared/tree';
 
 export function nodeIdentityFn(node: TreeNode<ResourceComponent>): string {
@@ -26,5 +30,10 @@ export class ExperiencesComponent implements OnInit {
     this.store.dispatch(actions.load({ kind: 'Context' }));
     this.store.dispatch(actions.load({ kind: 'Role' }));
     this.store.dispatch(actions.load({ kind: 'Impact' }));
+  }
+
+  addContext(): void {
+    const newContext = getFactory('Context')();
+    this.store.dispatch(actions.create({ resource: newContext }));
   }
 }
